0b5b40616d79707b3086ab774b7e728114dde082
RahulKrishnamoorthy0912 <123054346+RahulKrishnamoorthy0912@users.noreply.github.com>
2024-11-29 12:14:50 +0530

    Update streamlit_app_v2.0.py

--- streamlit_app_v2.0.py
+++ streamlit_app_v2.0.py
@@ -6,7 +6,7 @@
 
 import os
 import streamlit as st
-from llama_index import VectorStoreIndex
+from llama_index.core import VectorStoreIndex
 #from llama_index import load_index_from_storage
 #from llama_index.storage.storage_context import StorageContext
 
